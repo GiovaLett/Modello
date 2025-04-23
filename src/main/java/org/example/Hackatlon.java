41ea0edb89512761fb43245543f26ab74c1f06c5
Giovanni <>
2025-04-23 12:11:13 +0200

    Initial commit

---
+++ src/main/java/org/example/Hackatlon.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Hackatlon
+{
+    String Sede;
+    String Nome;
+    String data;   //vedere se cè un tipo data
+    String ID;
+
+
+    final int n_max_partec=100;
+    final int dim_team=3;
+
+    int n_team;
+    int durata=3;//giorni
+    int n_partec=0;
+
+
+
+
+}
---
+++ src/main/java/org/example/Utente.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Utente
+{
+    String nome;
+    String cognome;
+
+    public Utente(String nome,String cognome)
+    {
+        this.nome=nome;
+        this.cognome=cognome;
+    }
+
+    public void registrazione()
+    {
+
+    }
+}
