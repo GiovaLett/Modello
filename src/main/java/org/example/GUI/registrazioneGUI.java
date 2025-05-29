7820b6371b77dd24cf2dcf31a8b4addccfecee5d
Giovanni <>
2025-05-29 18:16:58 +0200

    creazione registrazioneGUI pt1

---
+++ src/main/java/org/example/GUI/registrazioneGUI.java
@@ -0,0 +1,13 @@
+package org.example.GUI;
+
+import javax.swing.*;
+
+public class registrazioneGUI {
+    private JPanel mainPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton registratiButton;
+}
